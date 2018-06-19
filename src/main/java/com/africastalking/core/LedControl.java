0dc9807c1810cbbe6ca9c850140c1e9ef86c6088
wnyabuto20 <wnyabuto20@amherst.edu>
2018-06-19 17:43:07 +0300

    updated xml3

--- src/main/java/com/africastalking/core/LedControl.java
+++ src/main/java/com/africastalking/core/LedControl.java
@@ -7,8 +7,7 @@ import com.pi4j.io.gpio.RaspiPin;
 
 public class LedControl {
 
-    public static void main(String[] args) //throws InterruptedException
-    {
+    public static void main(String[] args) throws InterruptedException {
 
         // get a handle to the GPIO controller
         final GpioController gpio = GpioFactory.getInstance();
@@ -19,13 +18,13 @@ public class LedControl {
         System.out.println("light is: ON");
 
         // wait 2 seconds
-        //Thread.sleep(2000);
+        Thread.sleep(2000);
 
         // turn off GPIO 1
         pin.toggle();
         System.out.println("light is: OFF");
         // wait 1 second
-       // Thread.sleep(1000);
+        Thread.sleep(1000);
         // turn on GPIO 1 for 1 second and then off
         System.out.println("light is: ON for 1 second");
         pin.pulse(1000, true);
